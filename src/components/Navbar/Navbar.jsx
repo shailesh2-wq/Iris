3d34483dc0172021f4a9251e54f3de7c95c91b52
chavan shailesh <chavanshailesh@chavans-MacBook-Air.local>
2025-10-23 17:50:45 +0530

    Responsiveness

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,12 @@
 import React, { useState } from "react";
 import "./Navbar.css";
-import { FaUniversity, FaGraduationCap, FaBuilding, FaGlobe, FaSearch, FaBars, FaTimes, FaCalendarAlt, FaCreditCard, FaUserFriends, FaUtensils } from "react-icons/fa";
+import { FaUniversity, FaGraduationCap, FaBuilding, FaGlobe, FaSearch, FaBars, FaTimes } from "react-icons/fa";
 import logo from "../../assets/assests/image.png";
 import userImage from "../../assets/assests/Studentimg.png";
 import img from "../../assets/assests/inst.png";
 import ed1 from "../../assets/assests/ed1.png";
 import bul from "../../assets/assests/building.png";
 import ear from "../../assets/assests/world.png";
-import cal from "../../assets/assests/calender.png";
-import fees from "../../assets/assests/wallet.png";
-import parent from "../../assets/assests/parent.png";
-import mes from "../../assets/assests/mess.png";
 
 const Navbar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
@@ -18,14 +14,6 @@ const Navbar = () => {
   const toggleMobileMenu = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
   };
-
-  // Quick actions data
-  const quickActions = [
-    { icon: cal, text: "View Timetable", action: () => console.log("View Timetable") },
-    { icon: fees, text: "Pay Fees", action: () => console.log("Pay Fees") },
-    { icon: parent, text: "Contact Advisor", action: () => console.log("Contact Advisor") },
-    { icon: mes, text: "Mess Change", action: () => console.log("Mess Change") }
-  ];
 
   return (
     <nav className="navbar">
@@ -53,23 +41,6 @@ const Navbar = () => {
             <FaSearch className="search-icon" />
           </div>
         </ul>
-
-        {/* Mobile Quick Actions */}
-        <div className="mobile-quick-actions">
-          <h4>Quick Actions</h4>
-          <div className="quick-actions-grid">
-            {quickActions.map((action, index) => (
-              <button 
-                key={index} 
-                className="quick-action-btn" 
-                onClick={action.action}
-              >
-                <img src={action.icon} alt={action.text} />
-                <span>{action.text}</span>
-              </button>
-            ))}
-          </div>
-        </div>
       </div>
 
       {/* Right User Info */}
